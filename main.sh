c14104065d4d66492df9e6f93abb1f12786a42a4
Eli Gladman <8713153+egladman@users.noreply.github.com>
2022-05-12 09:57:36 -0400

    use xz compression

--- main.sh
+++ main.sh
@@ -4,11 +4,16 @@ set -e
 
 trap cleanup ERR
 
+# REMIXER_OUTPUT_DIR
+# REMIXER_SQUASHFS_COMPRESSION
+
 iso_path="${1:?}"
+iso_squashfs_prefix=LiveOS
 cache_path="${PWD:?}/.cache"
 mount_path="${cache_path}/mnt"
 disk_path="${cache_path}/disk"
 root_path="${cache_path}/rootfs"
+
 
 log() {
     # Usage: log "message"
@@ -58,26 +63,31 @@ image_unpack() {
 
     cleanup
 
-    log "Uncompressing squashfs image '${disk_path}/LiveOS/squashfs.img' to path '${root_path}'"
-    unsquashfs -dest "${root_path}" "${disk_path}/LiveOS/squashfs.img"
+    log "Uncompressing squashfs image '${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img' to path '${root_path}'"
+    unsquashfs -dest "${root_path}" "${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img"
 }
 
 image_repack() {
     log "Creating squashfs from path '${root_path}'"
-    mksquashfs "${root_path}" "${cache_path}/squashfs.img" -noappend -always-use-fragments
+    mksquashfs "${root_path}" "${cache_path}/squashfs.img" -noappend -always-use-fragments -comp ${REMIXER_SQUASHFS_COMPRESSION:-xz}
 
     log "Overwritting existing squashfs image"
-    mv -f "${cache_path}/squashfs.img" "${disk_path}/LiveOS/squashfs.img"
+    mv -f "${cache_path}/squashfs.img" "${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img"
+
+    iso_custom_path="$(basename "${iso_path%%.iso}.remix.iso")"
+    if [ -n "$REMIXER_OUTPUT_DIR" ]; then # Save the remixed iso the same parent dir
+        iso_custom_path="${REMIXER_OUTPUT_DIR}/${iso_custom_path}"
+    fi
 
     log "Creating iso from path '${disk_path}'"
-    mkisofs -o "${iso_path%%.iso}.remix.iso" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "Custom Remix" "$disk_path"
+    mkisofs -o "$iso_custom_path" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "Custom Remix" "$disk_path"
 
     if [ -n "$SUDO_USER" ]; then
         log "Fixing permissions"
-        chown ${SUDO_USER}:${SUDO_USER} "${iso_path%%.iso}.remix.iso"
+        chown ${SUDO_USER}:${SUDO_USER} "$iso_custom_path"
     fi
 
-    log "Created ${iso_path%%.iso}.remix.iso"
+    log "Created iso '$iso_custom_path'"
 }
 
 image_modify() {
