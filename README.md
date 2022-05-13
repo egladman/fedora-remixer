9097c254196ce1cd32ff3e6f0a9b8fa7498b9105
Eli Gladman <8713153+egladman@users.noreply.github.com>
2022-05-13 00:22:15 -0400

    add ansible playbook

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Fedora Remixer
 
-Fedora remix builder written in POSIX shell
+Fedora remix builder written in Ansible and POSIX shell. Fork and modify the
+ansible playbooks to your heart's content.
 
 ## Dependencies
 
 - squashfs-tools
 - mkisofs
+- ansible
 
 ## Build
 
@@ -13,8 +15,19 @@ Fedora remix builder written in POSIX shell
 sudo ./main.sh <path/to/iso>
 ```
 
+*Note:* Elevated privileges are required since we're using chroots, and mounting loop devices.
+
 ## Cleanup
 
 ```
 sudo rm -rf .cache
 ```
+
+## Example
+
+```
+wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-1.5.iso
+sudo ./main.sh Fedora-Workstation-Live-x86_64-36-1.5.iso
+```
+
+*Note:* File `Fedora-Workstation-Live-x86_64-36-1.5.remix.iso` will be written to the working directory. This behavior can be overridden by setting environment variable `REMIXER_OUTPUT_DIR`.
--- main.sh
+++ main.sh
@@ -9,11 +9,15 @@ trap cleanup ERR
 
 iso_path="${1:?}"
 iso_squashfs_prefix=LiveOS
+iso_label="$(isoinfo -d -i "$iso_path" | sed -n 's/Volume id: //p')"
+ansible_path="${PWD:?}/ansible"
 cache_path="${PWD:?}/.cache"
 mount_path="${cache_path}/mnt"
-disk_path="${cache_path}/disk"
+disk_path="${cache_path}/disk" # Extracted iso contents
+squashfs_root_path="${cache_path}/squashfs" # Uncompressed squashfs
 root_path="${cache_path}/rootfs"
 
+skip_unpack=0
 
 log() {
     # Usage: log "message"
@@ -22,11 +26,18 @@ log() {
 
 cleanup() {
     # Usage: cleanup
-    log "Unmounting path '${mount_path}'"
-    umount "$mount_path"
+    log "Cleaning up"
+    umount "$mount_path" || true
+    umount "$root_path" || true
+
+    rm -f "${root_path}/etc/resolv.conf"
 }
 
 prep() {
+    if [ -d "$root_path" -a -s "$root_path" ]; then
+        skip_unpack=1
+    fi
+
     if [ ! -d "$cache_path" ]; then
         log "Creating directory '$cache_path'"
         mkdir -p "$cache_path"
@@ -49,10 +60,15 @@ prep() {
 }
 
 image_unpack() {
+    if [ $skip_unpack -eq 1 ]; then
+        log "Skipping unpacking"
+        return
+    fi
+
     log "Mounting iso '${iso_path}' to path '${mount_path}'"
     mount -t iso9660 -o loop "$iso_path" "$mount_path"
 
-    log "Entered directory '${mount_path}'"
+    log "Entering directory '${mount_path}'"
     pushd "$mount_path"
 
     log "Unpacking to path '$disk_path'"
@@ -61,15 +77,23 @@ image_unpack() {
     log "Leaving directory '${mount_path}'"
     popd
 
-    cleanup
+    log "Unmounting path '${mount_path}'"
+    umount "$mount_path"
 
-    log "Uncompressing squashfs image '${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img' to path '${root_path}'"
-    unsquashfs -dest "${root_path}" "${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img"
+    log "Uncompressing squashfs image '${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img' to path '${squashfs_root_path}'"
+    unsquashfs -dest "${squashfs_root_path}" "${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img"
+
+    log "Mounting rootfs image '${squashfs_root_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/rootfs.img' to path '${root_path}'"
+    mount -o loop "${squashfs_root_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/rootfs.img" "$root_path"
 }
 
 image_repack() {
+    # Mounted in previous step
+    log "Unmounting path '${root_path}'"
+    umount "$root_path" || true
+
     log "Creating squashfs from path '${root_path}'"
-    mksquashfs "${root_path}" "${cache_path}/squashfs.img" -noappend -always-use-fragments -comp ${REMIXER_SQUASHFS_COMPRESSION:-xz}
+    mksquashfs "${squashfs_root_path}" "${cache_path}/squashfs.img" -noappend -always-use-fragments -comp ${REMIXER_SQUASHFS_COMPRESSION:-xz}
 
     log "Overwritting existing squashfs image"
     mv -f "${cache_path}/squashfs.img" "${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img"
@@ -79,8 +103,12 @@ image_repack() {
         iso_custom_path="${REMIXER_OUTPUT_DIR}/${iso_custom_path}"
     fi
 
+    # FIXME
+    # Dracut will throw an error during boot, because it expects the original iso label i.e, Fedora-WS-Live-<major>-<minor>-<patch>
+    # For now we'll duplicate the source iso label
+
     log "Creating iso from path '${disk_path}'"
-    mkisofs -o "$iso_custom_path" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "Custom Remix" "$disk_path"
+    mkisofs -o "$iso_custom_path" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "$iso_label" "$disk_path"
 
     if [ -n "$SUDO_USER" ]; then
         log "Fixing permissions"
@@ -91,8 +119,24 @@ image_repack() {
 }
 
 image_modify() {
-    # todo
-    true
+    log "Entering directory '${ansible_path}'"
+    pushd "$ansible_path"
+
+    # FIXME
+    # Remove rsync dependency
+
+    # Allows the chroot to resolve DNS
+    log "Duplicating host resolv.conf to path '${root_path}/etc/resolv.conf'"
+    rsync -L --verbose /etc/resolv.conf "${root_path}/etc/"
+
+    log "Running ansible"
+    ansible-playbook --connection=chroot --inventory "${root_path}," site.yml
+
+    log "Removing path '${root_path}/etc/resolv.conf'"
+    rm -f "${root_path}/etc/resolv.conf"
+
+    log "Leaving directory '${ansible_path}'"
+    popd
 }
 
 main() {
