eb104fab16a47a4838154604a1c72f2d655ea59c
Eli Gladman <8713153+egladman@users.noreply.github.com>
2022-05-14 23:20:07 -0400

    support both i3 and sway

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Fedora Remixer
 
 Fedora remix builder written in Ansible and POSIX shell. Fork and modify the
-ansible playbooks to your heart's content.
+ansible playbook to your heart's content.
 
 ## Dependencies
 
@@ -9,13 +9,64 @@ ansible playbooks to your heart's content.
 - mkisofs
 - ansible
 
+## Setup
+
+```
+ansible-galaxy install -r ansible/requirements.yml
+```
+
 ## Build
+
+*Note:* Elevated privileges are required since we're using chroots, and mounting loop devices.
+
+| Environment Variable     | Description                                        | Default Value | Available Values                   |
+|--------------------------|----------------------------------------------------|---------------|------------------------------------|
+| RMX_OUTPUT_DIR           | Directory to save built iso                        | `""`          |                                    |
+| RMX_LIVE_PREFIX          | Directory where squashfs and rootfs `.img` reside  | `LiveOS`      |                                    |
+| RMX_SQUASHFS_COMPRESSION | Compression type used by `mksquashfs`              | `xz`          | `gzip`, `lzma`, `lzo`, `lz4`, `xz` |
+| RMX_ANSIBLE_TAGS         | Custom image be specifying additional ansible tags | `core`        | `core`, `core,devel`               |
+
+### Gnome
+
+#### Core
 
 ```
 sudo ./main.sh <path/to/iso>
 ```
 
-*Note:* Elevated privileges are required since we're using chroots, and mounting loop devices.
+#### Devel
+
+```
+sudo RMX_ANSIBLE_TAGS="core,devel" ./main.sh <path/to/iso>
+```
+
+### i3
+
+#### Core
+
+```
+sudo RMX_ANSIBLE_TAGS="core,i3" ./main.sh <path/to/iso>
+```
+
+#### Devel
+
+```
+sudo RMX_ANSIBLE_TAGS="core,i3,devel" ./main.sh <path/to/iso>
+```
+
+### Sway
+
+#### Core
+
+```
+sudo RMX_ANSIBLE_TAGS="core,sway" ./main.sh <path/to/iso>
+```
+
+#### Devel
+
+```
+sudo RMX_ANSIBLE_TAGS="core,sway,devel" ./main.sh <path/to/iso>
+```
 
 ## Cleanup
 
@@ -30,4 +81,18 @@ wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation
 sudo ./main.sh Fedora-Workstation-Live-x86_64-36-1.5.iso
 ```
 
-*Note:* File `Fedora-Workstation-Live-x86_64-36-1.5.remix.iso` will be written to the working directory. This behavior can be overridden by setting environment variable `REMIXER_OUTPUT_DIR`.
+*Note:* File `Fedora-Workstation-Live-x86_64-36-1.5.remix.iso` will be written to the working directory.
+
+## Troubleshooting
+
+1. `rm: cannot remove '.cache/rootfs': Device or resource busy`
+
+  ```
+  sudo umount .cache/rootfs
+  ```
+
+2. To manually mount the `rootfs.img` run the following. It is automatically unmounted when the script exits:
+
+  ```
+  sudo mount -o loop .cache/squashfs/LiveOS/rootfs.img .cache/rootfs
+  ```
--- main.sh
+++ main.sh
@@ -4,11 +4,15 @@ set -e
 
 trap cleanup ERR
 
-# REMIXER_OUTPUT_DIR
-# REMIXER_SQUASHFS_COMPRESSION
+# RMX_OUTPUT_DIR
+# RMX_LIVE_PREFIX
+# RMX_SQUASHFS_COMPRESSION
+# RMX_ANSIBLE_TAGS
+
+RMX_LIVE_PREFIX="${RMX_LIVE_PREFIX:-LiveOS}"
+RMX_ANSIBLE_TAGS="${RMX_ANSIBLE_TAGS:-core}"
 
 iso_path="${1:?}"
-iso_squashfs_prefix=LiveOS
 iso_label="$(isoinfo -d -i "$iso_path" | sed -n 's/Volume id: //p')"
 ansible_path="${PWD:?}/ansible"
 cache_path="${PWD:?}/.cache"
@@ -59,6 +63,15 @@ prep() {
     fi
 }
 
+post() {
+    if [ -z "$SUDO_USER" ]; then
+        return
+    fi
+
+    log "Fixing permissions for path '${cache_path}'"
+    chown ${SUDO_USER}:${SUDO_USER} "$cache_path" "$root_path"
+}
+
 image_unpack() {
     if [ $skip_unpack -eq 1 ]; then
         log "Skipping unpacking"
@@ -80,11 +93,11 @@ image_unpack() {
     log "Unmounting path '${mount_path}'"
     umount "$mount_path"
 
-    log "Uncompressing squashfs image '${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img' to path '${squashfs_root_path}'"
-    unsquashfs -dest "${squashfs_root_path}" "${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img"
+    log "Uncompressing squashfs image '${disk_path}/${RMX_LIVE_PREFIX}/squashfs.img' to path '${squashfs_root_path}'"
+    unsquashfs -dest "${squashfs_root_path}" "${disk_path}/${RMX_LIVE_PREFIX}/squashfs.img"
 
-    log "Mounting rootfs image '${squashfs_root_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/rootfs.img' to path '${root_path}'"
-    mount -o loop "${squashfs_root_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/rootfs.img" "$root_path"
+    log "Mounting rootfs image '${squashfs_root_path}/${RMX_LIVE_PREFIX}/rootfs.img' to path '${root_path}'"
+    mount -o loop "${squashfs_root_path}/${RMX_LIVE_PREFIX}/rootfs.img" "$root_path"
 }
 
 image_repack() {
@@ -93,14 +106,14 @@ image_repack() {
     umount "$root_path" || true
 
     log "Creating squashfs from path '${root_path}'"
-    mksquashfs "${squashfs_root_path}" "${cache_path}/squashfs.img" -noappend -always-use-fragments -comp ${REMIXER_SQUASHFS_COMPRESSION:-xz}
+    mksquashfs "${squashfs_root_path}" "${cache_path}/squashfs.img" -noappend -always-use-fragments -comp ${RMX_SQUASHFS_COMPRESSION:-xz}
 
     log "Overwritting existing squashfs image"
-    mv -f "${cache_path}/squashfs.img" "${disk_path}/${REMIXER_ISO_SQUASHFS_PREFIX:-$iso_squashfs_prefix}/squashfs.img"
+    mv -f "${cache_path}/squashfs.img" "${disk_path}/${RMX_LIVE_PREFIX}/squashfs.img"
 
     iso_custom_path="$(basename "${iso_path%%.iso}.remix.iso")"
-    if [ -n "$REMIXER_OUTPUT_DIR" ]; then # Save the remixed iso the same parent dir
-        iso_custom_path="${REMIXER_OUTPUT_DIR}/${iso_custom_path}"
+    if [ -n "$RMX_OUTPUT_DIR" ]; then # Save the remixed iso the same parent dir
+        iso_custom_path="${RMX_OUTPUT_DIR}/${iso_custom_path}"
     fi
 
     # FIXME
@@ -111,7 +124,7 @@ image_repack() {
     mkisofs -o "$iso_custom_path" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "$iso_label" "$disk_path"
 
     if [ -n "$SUDO_USER" ]; then
-        log "Fixing permissions"
+        log "Fixing permissions for path '${iso_custom_path}'"
         chown ${SUDO_USER}:${SUDO_USER} "$iso_custom_path"
     fi
 
@@ -129,8 +142,20 @@ image_modify() {
     log "Duplicating host resolv.conf to path '${root_path}/etc/resolv.conf'"
     rsync -L --verbose /etc/resolv.conf "${root_path}/etc/"
 
+
+    if [ -n "$SUDO_USER" ]; then
+        # This is necessary since we're running the script as root, and the
+        # ansible looks for modules/collections in the current user's home.
+        # Run the following to learn more:
+        #   ansible-config dump | grep DEFAULT_MODULE_PATH
+        #   ansible-config dump | grep COLLECTION_PATHS
+        log "Overriding Ansible default paths for modules and collections"
+        export ANSIBLE_LIBRARY="/home/${SUDO_USER}/.ansible/plugins/modules:/usr/share/ansible/plugins/modules"
+        export ANSIBLE_COLLECTIONS_PATHS="/home/${SUDO_USER}/.ansible/collections:/usr/share/ansible/collections"
+    fi
+
     log "Running ansible"
-    ansible-playbook --connection=chroot --inventory "${root_path}," site.yml
+    ansible-playbook --connection=chroot --inventory "${root_path}," --tags "${RMX_ANSIBLE_TAGS}" site.yml
 
     log "Removing path '${root_path}/etc/resolv.conf'"
     rm -f "${root_path}/etc/resolv.conf"
@@ -144,6 +169,7 @@ main() {
     image_unpack
     image_modify
     image_repack
+    post
 }
 
 main
